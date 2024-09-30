949bd3ffc3fe795751149d9974c10d50c8fa9f41
Daniel Villera <davillera77@gmail.com>
2024-09-30 18:52:22 -0500

    setup primeng and primeflex

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,18 @@
-import { ApplicationConfig } from '@angular/core';
+import {ApplicationConfig, importProvidersFrom} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import {BrowserModule} from "@angular/platform-browser";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {MessageService} from "primeng/api";
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    MessageService,
+    importProvidersFrom(
+      BrowserModule,
+      BrowserAnimationsModule,
+    ),
+    provideRouter(routes)
+  ]
 };
