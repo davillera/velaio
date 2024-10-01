f1e9a46c1a2630d7ed80ae3db21711922dbb30be
Daniel Villera <davillera77@gmail.com>
2024-10-01 16:49:17 -0500

    not found

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
+import {NotFoundComponent} from "./pages/not-found/not-found.component";
 
 export const routes: Routes = [
   {
     path: '',
     loadComponent: () => import('./pages/home/home.component').then(m => m.HomeComponent),
     pathMatch: 'full',
+  },
+  {
+    path: '**',
+    component: NotFoundComponent
   }
 ];
